440aac7a60bd592482939e9a35b56002f17ee4c2
faridisayev <faridisayev3@icloud.com>
2023-06-16 16:43:44 +0300

    Add update account feature

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from app.models.account import Account
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(max=20)])
+    email = EmailField(validators=[InputRequired(), Length(max=80)])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        existing_username = Account.query.filter_by(username = username.data).first()
+        if existing_username: raise ValidationError('An account with this username already exists.')
+
+    def validate_email(self, email):
+        existing_email = Account.query.filter_by(email = email.data).first()
+        if existing_email: raise ValidationError('An account with this email already exists.')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect, url_for
 from app.main import bp 
-from flask_login import login_required, logout_user, current_user
+from flask_login import login_required, logout_user, current_user, fresh_login_required
 from app.extensions import db
 from app.models.account import Account
+from app.main.forms import UpdateAccountForm
 
 @bp.route('/')
 def home():
@@ -32,3 +33,14 @@ def delete_account(id):
     db.session.commit()
     flash('Your account was successfully deleted.', 'success')
     return redirect(url_for('auth.login'))
+
+@bp.route('/update_account', methods = ['GET', 'POST'])
+@fresh_login_required
+def update_account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated.', 'success')
+    return render_template('update_account.html', form = form)
